27ec977d08a74d266abd8b93fafebcc4ab4dca41
nauflabhy <n4uf4l.ku@gmail.com>
2025-05-27 21:03:29 +0700

    Memasukan member, constructor, descturctor untuk class manusia

--- Komposisi/manusia.h
+++ Komposisi/manusia.h
@@ -1,8 +1,19 @@
+#include <iostream>
 #ifndef MANUSIA_H
 #define MANUSIA_H
+#include "jantung.h"
 using namespace std;
 
 class manusia {
     public:
+        string name;
+        jantung varJantung;
+
+        manusia(string pName): name(pName) {
+            cout << name << " hidup\n";
+        }
+        ~manusia() {
+            cout << name << " mati\n";
+        }
 };
 #endif
