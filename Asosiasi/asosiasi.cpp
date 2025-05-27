0d2a78c91892ca07ed1b011b0a307feff68b9b07
nauflabhy <n4uf4l.ku@gmail.com>
2025-05-27 18:48:59 +0700

    Membuat member class dokter beserta fungsinya

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -24,4 +24,18 @@ class pasien{
 
 class dokter {
     public:
+        string nama;
+        vector<pasien*> daftar_pasien;
+
+        dokter(string pNama): nama(pNama) {
+            cout << "Dokter \"" << nama << "\" ada\n";
+        }
+
+        ~dokter() {
+            cout << "Dokter \"" << nama
+            << "\" tidak ada\n";
+        }
+
+        void tambahPasien(pasien*);
+        void cetakPasien();
 };
